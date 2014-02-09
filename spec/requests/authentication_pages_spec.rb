21ca2033c3b58ab05ccabf4a0bfe4d385a24027d
shiljos <shiljos@gmail.com>
2014-02-09 17:08:29 +0100

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -90,7 +90,22 @@ describe "AuthenticationPages" do
           before { delete micropost_path(FactoryGirl.create(:micropost)) }
           specify { expect(response).to redirect_to(signin_path) }
         end
+      end#////////////
+
+
+	describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end	
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
       end
+
+
 
 		describe "when attempting to visit a protected page" do
           before do
@@ -109,6 +124,17 @@ describe "AuthenticationPages" do
       end
 
       describe "in the Users controller" do
+
+
+		describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
 
       	describe "visiting the user index" do
           before { visit users_path }
